767f8882732c074c2e1e517b657ab3326fa0b4d4
soumalyab28 <soumalyab28@gmail.com>
2022-07-18 22:46:26 +0530

    Added Comments

--- guessing_game.py
+++ guessing_game.py
@@ -14,7 +14,8 @@ num = random.randint(10,50)
 
 flag = 101
 guess_cnt = 0
-#print(num)
+guess_prev = 0
+print(num)
 
 print("\n")
 print("----------------------------------------------------")
@@ -43,10 +44,9 @@ while flag == 101:
             print("WOW Congratulations! It's a MATCH. You have guessed it in FIRST attempt itself.")
         else:
             print("Congratulations! It's a MATCH. You have guessed it in "+str(guess_cnt)+" attempts")
-        flag = 102
-    
-    if num > guess:
-        if ((num - guess) <= 5):
+        break
+
+    if (abs(num - guess) <= 5):
             if guess_cnt == 1:
                 print("WARM")              
             elif guess_cnt > 1:
@@ -54,7 +54,7 @@ while flag == 101:
                     print("COLDER")
                 elif guess > guess_prev:
                     print("WARMER")
-        elif ((num - guess) > 5):
+    elif (abs(num - guess) > 5):
             if guess_cnt == 1:
                 print("COLD")
             elif guess_cnt > 1:
@@ -62,28 +62,41 @@ while flag == 101:
                     print("COLDER")
                 elif guess > guess_prev:
                     print("WARMER")
-    if num < guess:
-        if ((guess - num) <= 5):
-            if guess_cnt == 1:
-                print("WARM")              
-            elif guess_cnt > 1:
-                if guess < guess_prev:
-                    print("WARMER")
-                elif guess > guess_prev:
-                    print("COLDER")
-        elif ((guess - num) > 5):
-            if guess_cnt == 1:
-                print("COLD")
-            elif guess_cnt > 1:
-                if guess < guess_prev:
-                    print("WARMER")
-                elif guess > guess_prev:
-                    print("COLDER")
     guess_prev = guess
 
+
+# Alternate soluation using Lists
+# Hint: zero is a good placeholder value. It's useful because it evaluates to "False".
+# So we create a list with just 0 element in it
+# guesses = [0]
+# while True:
+
+#     # we can copy the code from above to take an input
+#     guess = int(input("I'm thinking of a number between 1 and 100.\n  What is your guess? "))
     
-
-
+#     if guess < 1 or guess > 100:
+#         print('OUT OF BOUNDS! Please try again: ')
+#         continue
     
-
-
+#     # here we compare the player's guess to our number
+#     if guess == num:
+#         print(f'CONGRATULATIONS, YOU GUESSED IT IN ONLY {len(guesses)} GUESSES!!')
+#         break
+        
+#     # if guess is incorrect, add guess to the list
+#     guesses.append(guess)
+    
+#     # when testing the first guess, guesses[-2]==0, which evaluates to False
+#     # and brings us down to the second section
+    
+#     if guesses[-2]:  
+#         if abs(num-guess) < abs(num-guesses[-2]):
+#             print('WARMER!')
+#         else:
+#             print('COLDER!')
+   
+#     else:
+#         if abs(num-guess) <= 10:
+#             print('WARM!')
+#         else:
+#             print('COLD!')
